d13ec7f1d2953fedc49a5d412e279968e90dd48a
sabrinamaral <sabrinamaral@hotmail.com>
2022-10-24 22:21:00 -0300

    Register page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Wrapper from "../assets/wrappers/RegisterPage";
+import { Logo, FormRow } from "../components";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
 
 const Register = () => {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={handleSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        {/* name field */}
+        <FormRow
+          type="text"
+          name="name"
+          value={values.name}
+          handleChange={handleChange}
+        />
+        {/* email field */}
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* password field */}
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button type="submit" className="btn btn-block" onSubmit={handleSubmit}>
+          submit
+        </button>
+        <p className="coffee-info">
+          Not a member yet? <a> Register</a>
+        </p>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Register;
