d96b85293c98f3e4a39dcd82fc54815ed5be821d
sabrinamaral <sabrinamaral@hotmail.com>
2022-10-26 08:27:17 -0300

    React Toastify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import { Landing, Error, Dashboard, Register } from "./pages";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Landing, Error, Dashboard, Register } from "./pages";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   return (
     <BrowserRouter>
@@ -9,6 +12,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="*" element={<Error />} />
       </Routes>
+      <ToastContainer position="top-center" />
     </BrowserRouter>
   );
 }
