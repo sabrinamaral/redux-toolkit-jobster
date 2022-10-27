c52c048b5093da8697ae1ccd108949965dd85825
sabrinamaral <sabrinamaral@hotmail.com>
2022-10-27 12:03:39 -0300

    local storage

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -2,10 +2,15 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { toast } from "react-toastify";
 import customFetch from "../../utils/axios";
+import {
+  addUserToLocalStorage,
+  getUserFromLocalStorage,
+  removeUserFromLocalStorage,
+} from "../../utils/localStorage";
 
 const initialState = {
   isLoading: false,
-  user: null,
+  user: getUserFromLocalStorage(),
 };
 
 export const registerUser = createAsyncThunk(
@@ -23,7 +28,13 @@ export const registerUser = createAsyncThunk(
 export const loginUser = createAsyncThunk(
   "user/loginUser",
   async (user, thunkAPI) => {
-    console.log(`Login User : ${user}`);
+    try {
+      const resp = await customFetch.post("/auth/login ", user);
+      return resp.data;
+    } catch (error) {
+      console.log(error);
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
   }
 );
 
@@ -38,9 +49,24 @@ const userSlice = createSlice({
       const { user } = payload;
       state.isLoading = false;
       state.user = user;
+      addUserToLocalStorage(user);
       toast.success(`Hello There ${user.name}`);
     },
     [registerUser.rejected]: (state, { payload }) => {
+      state.isLoading = false;
+      toast.error(payload);
+    },
+    [loginUser.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [loginUser.fulfilled]: (state, { payload }) => {
+      const { user } = payload;
+      state.isLoading = false;
+      state.user = user;
+      addUserToLocalStorage(user);
+      toast.success(`Welcome back ${user.name}`);
+    },
+    [loginUser.rejected]: (state, { payload }) => {
       state.isLoading = false;
       toast.error(payload);
     },
---
+++ src/utils/localStorage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const addUserToLocalStorage = (user) => {
+  localStorage.setItem("user", JSON.stringify(user));
+};
+
+export const removeUserFromLocalStorage = () => {
+  localStorage.removeItem("user");
+};
+
+export const getUserFromLocalStorage = () => {
+  const result = localStorage.getItem("user");
+  const user = result && JSON.parse(result);
+  return user;
+};
