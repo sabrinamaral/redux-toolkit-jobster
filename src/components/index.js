f7e3dca0e702c11880df4144ec23706b38c795be
sabrinamaral <sabrinamaral@hotmail.com>
2022-11-01 22:54:24 -0300

    NavLinks component

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import FormRow from "./FormRow";
 import Navbar from "./Navbar";
 import SmallSideBar from "./SmallSidebar";
 import BigSidebar from "./BigSidebar";
+import NavLinks from "./NavLinks";
 
-export { BigSidebar, FormRow, Navbar, Logo, SmallSideBar };
+export { BigSidebar, FormRow, Navbar, NavLinks, Logo, SmallSideBar };
