66b9f1f18c2ca4d4b6a4bbd2684140b89bfc0b14
sabrinamaral <sabrinamaral@hotmail.com>
2022-11-02 11:17:17 -0300

    Profile page

--- src/pages/dashboard/Profile.js
+++ src/pages/dashboard/Profile.js
@@ -1,7 +1,71 @@
-import React from "react";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Wrapper from "../../assets/wrappers/DashboardFormPage";
+import { FormRow } from "../../components";
+import { toast } from "react-toastify";
 
 const Profile = () => {
-  return <h1>Profile</h1>;
+  const { isLoading, user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const [userData, setUserData] = useState({
+    name: user?.name || "",
+    lastName: user?.lastName || "",
+    email: user?.email || "",
+    location: user?.location || "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, lastName, email, location } = userData;
+    if (!name || !lastName || !email || !location) {
+      toast.error("Please fill out all fields!");
+      return;
+    }
+  };
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  return (
+    <Wrapper>
+      <form className="form" onSubmit={handleSubmit}>
+        <h3>profile</h3>
+        <div className="form-center">
+          <FormRow
+            type="text"
+            name="name"
+            value={userData.name}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type="text"
+            name="lastName"
+            labelName="last name"
+            value={userData.lastName}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type="email"
+            name="email"
+            value={userData.email}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type="location"
+            name="location"
+            value={userData.location}
+            handleChange={handleChange}
+          />
+          <button type="submit" className="btn btn-block" disabled={isLoading}>
+            {isLoading ? "please wait..." : "save changes"}
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Profile;
