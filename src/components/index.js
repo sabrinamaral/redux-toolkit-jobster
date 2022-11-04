ec0a13ec4883967325a626818a0daaacd69b3d61
sabrinamaral <sabrinamaral@hotmail.com>
2022-11-04 09:04:33 -0300

    boilerplate and index | component

--- src/components/index.js
+++ src/components/index.js
@@ -5,13 +5,21 @@ import SmallSideBar from "./SmallSidebar";
 import BigSidebar from "./BigSidebar";
 import NavLinks from "./NavLinks";
 import FormRowSelect from "./FormRowSelect";
+import SearchContainer from "./SearchContainer";
+import JobsContainer from "./JobsContainer";
+import Job from "./Job";
+import Loading from "./Loading";
 
 export {
   BigSidebar,
   FormRow,
   Navbar,
   NavLinks,
+  Loading,
   Logo,
   SmallSideBar,
   FormRowSelect,
+  SearchContainer,
+  JobsContainer,
+  Job,
 };
