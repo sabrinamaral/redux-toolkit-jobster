d10a0159817072f65386c1022890ed94a115130b
sabrinamaral <sabrinamaral@hotmail.com>
2022-11-04 22:55:24 -0300

    useEffect | getAllJobs at JobsContainer

--- src/components/JobsContainer.js
+++ src/components/JobsContainer.js
@@ -2,10 +2,16 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Wrapper from "../assets/wrappers/JobsContainer";
 import { Job, Loading } from "../components";
+import { getAllJobs } from "../features/allJobs/allJobsSlice";
 
 const JobsContainer = () => {
   const { isLoading, jobs } = useSelector((store) => store.allJobs);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllJobs());
+  }, []);
+
   if (isLoading) {
     return (
       <Wrapper>
@@ -25,7 +31,7 @@ const JobsContainer = () => {
       <h5>jobs info</h5>
       <div className="jobs">
         {jobs.map((job) => {
-          return <Job key={job._key} {...job} />;
+          return <Job key={job._id} {...job} />;
         })}
       </div>
     </Wrapper>
